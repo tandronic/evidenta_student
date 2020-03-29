7b99b3a612884ac3ac263bc15b41c239c0ede064
Tudor Andronic <andronictudor@outlook.com>
2020-03-29 13:00:07 +0300

    Added project and gitignore file

---
+++ EvidentaStudenti/Program.cs
@@ -0,0 +1,99 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s = new Student("Ionescu", "Mihai");
+            Menu(s, args);
+        }
+
+        static void Menu(Student s, string[] args)
+        {
+            string menu = "S: Citire note de la tastura ca un sir de caractere\n";
+            menu += "T: Citire note de la tastatura (cate o nota pe rand)\n";
+            menu += "C: Citire note din linia de comanda\n";
+            menu += "A: Afisare note\n";
+            menu += "I: Info autor\n";
+            menu += "F: Citire format fisier(id, Prenuma, Nume, Note)\n";
+            menu += "X: Iesire\n";
+            do
+            {
+                Console.WriteLine(menu);
+                string optiune = Console.ReadLine();
+                optiune = optiune.ToUpper();
+                switch (optiune)
+                {
+                    case "S":
+                        var note_citite = Console.ReadLine();
+                        s.SetNote(note_citite);
+                        Console.ReadKey();
+                        break;
+                    case "A":
+                        Console.WriteLine(s.ConversieLaSir());
+                        s.NumarareNote(out int valSub5,out int valPeste5);
+                        Console.WriteLine("\nNumarul notelor sub 5 este: " + valSub5);
+                        Console.WriteLine("\nNumarul notelor peste 5 este: " + valPeste5);
+                        Console.ReadKey();
+                        break;
+                    case "T":
+                        int n, nota;
+                        int[] note;
+                        string str_n;
+                        do
+                        {
+                            Console.WriteLine("Introdu numarul de note: ");
+                            str_n = Console.ReadLine();
+                        }while(!System.Int32.TryParse(str_n, out n));
+                        note = new int[n];
+                        int j = 0;
+                        for(int i=0; i<n; i++)
+                        {
+                            bool Rez = System.Int32.TryParse(Console.ReadLine(), out nota); 
+                            if(Rez)
+                            {
+                                note[j] = nota;
+                                j++;
+                            }
+                        }
+                        s.SetNote(note);
+                        Console.ReadKey();
+                        break;
+                    case "C":
+                        s.SetNote(String.Join(" ", args));
+                        Console.ReadKey();
+                        break;
+                    case "I":
+                        Console.WriteLine("Andronic Tudor\nGrupa 3121 A");
+                        Console.ReadKey();
+                        break;
+                    case "F":
+                        string date_fisier;
+                        string[] date;
+                        do
+                        {
+                            date_fisier = Console.ReadLine();
+                            date = date_fisier.Split(',');
+                            if (date.Length != 4)
+                                Console.WriteLine("Date introduse nu sunt valide\n");
+                        } while (date.Length != 4);
+                        Student s_fisier = new Student(date_fisier);
+                        Console.WriteLine(s_fisier.ConversieLaSir());
+                        Console.ReadKey();
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiunea aleasa nu este valida");
+                        return;
+                }
+                Console.Clear();
+            } while (true);
+        }
+    }
+}
---
+++ LibrarieEntitati/Student.cs
@@ -0,0 +1,89 @@
+﻿namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume, prenume;
+        int[] note;
+        int id;
+
+        // contructor implicit
+        public Student()
+        {
+        }
+
+        // constructor cu parametri
+        public Student(string _nume, string _prenume)
+        {
+            nume = _nume;
+            prenume = _prenume;
+        }
+
+        public Student(string date_fisier)
+        {
+            string[] date;
+            date = date_fisier.Split(',');
+            id = System.Convert.ToInt32(date[0]);
+            prenume = date[1];
+            nume = date[2];
+            SetNote(date[3]);
+        }
+
+        public void SetNote(string sirNote)
+        {
+            string[] sir_note = sirNote.Split(' ');
+            note = new int[sir_note.Length];
+            int i = 0;
+            foreach(var nota in sir_note)
+            {
+                int n = 0;
+                bool Rez = System.Int32.TryParse(nota, out n);
+                if (Rez == true)
+                {
+                    int nota_int = System.Int32.Parse(nota);
+                    if (nota_int > 0 && nota_int <= 10)
+                    {
+                        note[i] = nota_int;
+                        i++;
+                    }
+                }
+            }
+            System.Array.Resize(ref note, i);
+        }
+
+        public void SetNote(int[] _note)
+        {
+            note = new int[_note.Length];
+            for(int i=0;  i< _note.Length; i++)
+            {
+                note[i] = _note[i];
+            }
+        }
+        public string ConversieLaSir()
+        {
+            string sNote = "Nu exista (Nu ati apelat metoda **setNote**)";
+            string nume_complet = nume;
+            if (note != null)
+            {
+                sNote = string.Join(",", note);
+            }
+            if (string.IsNullOrEmpty(prenume) == false)
+                nume_complet += " " + prenume;
+            string s = string.Format("Studentul {0} are notele:.... {1}", nume_complet, sNote);
+
+            return s;
+        }
+
+        public void NumarareNote(out int valSub5,out int valPeste5)
+        {
+            valSub5 = valPeste5 = 0;
+            for(int i=0; i<note.Length; i++)
+            {
+                if (note[i] < 5)
+                    valSub5++;
+                else
+                    valPeste5++;
+            }
+        }
+    }
+}
